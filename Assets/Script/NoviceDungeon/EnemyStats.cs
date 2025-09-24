8c79c5ea13d61520b510280dcf32848827627a61
DESKTOP-PFILKRM\marz anoya <espie.37743@gmail.com>
2025-09-24 22:01:57 +0800

    Pakidelete yung previous push ko sa novice dungeon. Ito yung totoong novice dungeon. With UI and scripts pero di pa natetest yung flow...

---
+++ Assets/Script/NoviceDungeon/EnemyStats.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EnemyStats : MonoBehaviour
+{
+    public string enemyName = "Enemy";
+    public int maxHP = 50;
+    public int attackdamage = 10;
+    public float attackInterval = 10f;
+    public int currentHP;
+
+    public Slider hpSlider;
+    public TMP_Text nameText;
+    public TMP_Text timerText;
+
+    void Awake()
+    {
+        currentHP = maxHP;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHP -= amount;
+        if (currentHP < 0)
+        {
+            currentHP = 0;
+        }
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        if (hpSlider != null)
+        {
+            hpSlider.maxValue = maxHP;
+            hpSlider.value = currentHP;
+        }
+
+        if (nameText != null)
+        {
+            nameText.text = enemyName;
+        }
+    }
+}
