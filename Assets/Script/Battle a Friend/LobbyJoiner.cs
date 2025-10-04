4ea3ff5ea2fc5b4fa35025336ca91c814315e9f0
kujojotarooo0000-admin <kujojotarooo0000@gmail.com>
2025-10-04 11:11:39 +0800

    Fixed the Portal, Debugged the Multiplayer lobby, added Versions of Portal UI

--- Assets/Script/Battle a Friend/LobbyJoiner.cs
+++ Assets/Script/Battle a Friend/LobbyJoiner.cs
@@ -2,11 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using UnityEngine;
 using UnityEngine.UI;
 
 public class LobbyJoiner : MonoBehaviour
--- Assets/Script/Battle a Friend/MultiplayerManager.cs
+++ Assets/Script/Battle a Friend/MultiplayerManager.cs
@@ -1,51 +1,36 @@
-using System.Collections.Generic;
-using TMPro;
-using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MultiplayerManager : MonoBehaviour
 {
-    // Reference to the input field for the join code
+    // Reference to the pop-up panel for joining a lobby
+    public GameObject joinLobbyPopup;
     public TMP_InputField joinCodeInput;
-    public List<GameObject> playerSlots = new List<GameObject>(); // Use a list for player slots
 
-    // For this example, we'll use a simple random number
-    private string hostCode;
-
-    void Start()
+    // Call this when the "Battle a Friend" button is clicked in the ARScene
+    public void GoToRoomScene()
     {
-        GenerateHostCode();
-        UpdatePlayerSlots();
+        SceneManager.LoadScene("Room");
     }
 
-    void GenerateHostCode()
-    {
-        hostCode = Random.Range(1000, 9999).ToString();
-        // This would be displayed in the Host Lobby scene
-        Debug.Log($"Host Code: {hostCode}");
-    }
-
-    void UpdatePlayerSlots()
-    {
-        // In a real project, this would be updated by a network event
-        // For now, we'll just show the host
-        playerSlots[0].SetActive(true);
-        playerSlots[1].SetActive(false);
-    }
-
-    // Call this when the "Host a Battle" button is clicked
+    // Call this when the "Search a Friend" button is clicked in the Room scene
     public void HostBattle()
     {
-        NetworkManager.Singleton.StartHost();
         Debug.Log("Hosting a new battle...");
         SceneManager.LoadScene("SearchAFriend");
     }
 
-    // Call this when the "Join a Battle" button is clicked
+    // Call this when the "Join a Friend" button is clicked
+    public void ShowJoinPopup()
+    {
+        joinLobbyPopup.SetActive(true);
+    }
+
+    // Called when player confirms the code in the pop-up
     public void JoinBattle(string hostCode)
     {
         Debug.Log($"Attempting to join lobby with code: {hostCode}");
-        SceneManager.LoadScene("SearchAFriend");
+        SceneManager.LoadScene("TheJoinAFriend");
     }
 }
