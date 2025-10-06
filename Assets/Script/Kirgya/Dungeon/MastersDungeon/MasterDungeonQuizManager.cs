5ad92d17e366e4cb32feaa21065d55950b2b00b8
DESKTOP-PFILKRM\marz anoya <espie.37743@gmail.com>
2025-10-06 15:52:09 +0800

    Fully functional Masters Dungeon FINALLY. Also some minor changes I forgot to do in Advanced...

--- Assets/Script/Kirgya/Dungeon/MastersDungeon/MasterDungeonQuizManager.cs
+++ Assets/Script/Kirgya/Dungeon/MastersDungeon/MasterDungeonQuizManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5a9cd6f65c863e8e01677f07a2fe99305b29a45d1e42b6056f0eb6f03c41353
-size 15426
+oid sha256:5ff1c86d81a3f68d8dc058f2e6f09aae2f950458820520474e01e450b531680b
+size 14935
---
+++ Assets/Script/Kirgya/Dungeon/MastersDungeon/MastersBossSpawner.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7169fea9986539e38667ee354d7d15788ce6bf77e0f66dca56cb984e3969d934
+size 3006
