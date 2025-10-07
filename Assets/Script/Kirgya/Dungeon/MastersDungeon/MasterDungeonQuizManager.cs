1e18dee54fb80f8438e129aba1655f657540f883
DESKTOP-PFILKRM\marz anoya <espie.37743@gmail.com>
2025-10-07 23:38:01 +0800

    Finished and checked the functionalities of all weapons...

--- Assets/Script/Kirgya/Dungeon/MastersDungeon/MasterDungeonQuizManager.cs
+++ Assets/Script/Kirgya/Dungeon/MastersDungeon/MasterDungeonQuizManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:893c2a00d8aa5613c86d496e3a46d3df3c54d1b5a3d271d6e8897943cff5d25f
-size 15442
+oid sha256:e8be6ef06f2830c8407db2b16fb89ecfedbbd7d7c8da1e37584caa2ddf82492f
+size 24071
--- Assets/Script/Kirgya/Dungeon/QuestionData.cs
+++ Assets/Script/Kirgya/Dungeon/QuestionData.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:be90c3ffd9d7c654045fede64a405528bd618ae3e97896ca946bd236d6e055e8
-size 1493
+oid sha256:cf148e642e5f9840ea6298e3527a3f25a31e68fd1b81f2b872d92f51b18bc969
+size 1862
