edd2135a2d2da3a31cbfbd66c306849deb5085cb
DESKTOP-PFILKRM\marz anoya <espie.37743@gmail.com>
2025-10-10 03:04:38 +0800

    Finally made the items be saved on the buttons during item selection before starting the dungeon...

--- Assets/Script/Kirgya/Dungeon/DungeonWeaponSelection/DungeonSelectButton.cs
+++ Assets/Script/Kirgya/Dungeon/DungeonWeaponSelection/DungeonSelectButton.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a789d72f1dcc7f8f7ef93398e9008bfe833a73aca2589a9cbb5617055f415413
-size 1729
+oid sha256:bcd8266cd349ff4a86333074dbd2a96f4d93c10b9803c3a9b6e03321402e6918
+size 1866
