edacbe721eb5bfe8e9993dcb4a3939584cc0fef3
kujojotarooo0000-admin <kujojotarooo0000@gmail.com>
2025-10-10 01:18:48 +0800

    Modified AR CAMERA across the scenes, modified profile data to the Profile Scene.

--- Assets/Script/Kirgya/ProfileManager.cs
+++ Assets/Script/Kirgya/ProfileManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6542f2249fb48238523cd1b78988552d59bca346f11fcc18a50ea637640b706a
-size 7099
+oid sha256:874ad355ac5a5f85191ba0ddbca6ff1d6f71aca8231a207b28bd13d8dd1e90a1
+size 5420
