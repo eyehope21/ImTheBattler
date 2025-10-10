b3d4ade6134ac3327ceaed07025fc801d9a1c6f7
DESKTOP-PFILKRM\marz anoya <espie.37743@gmail.com>
2025-10-10 15:31:24 +0800

    Tapos na yung system, pwede na rin magrecover ng item usage sa rest panel. Armor effects nalang idadagdag tas ikakabit ko na sa dungeons ng advanced at masters...

--- Assets/Script/Kirgya/Dungeon/NoviceDungeon/NoviceBattleManager.cs
+++ Assets/Script/Kirgya/Dungeon/NoviceDungeon/NoviceBattleManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08eb0897af24bf283a2ea7be3213bd93c0bff4fa4e74c91cdbb9645e93856bef
-size 15388
+oid sha256:ff8c00cb9d99f2bb4ab0b77c7beea40e10141ad4d02180dc623470b26a3e492d
+size 14472
--- Assets/Script/Kirgya/GameManager.cs
+++ Assets/Script/Kirgya/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02274849079a1920fa564067cdeafbb6a1dd2fba2fd27b0c69729fb77e0cf4d0
-size 2078
+oid sha256:ec5a67ab521ffada0f73fa9c5cc02ac83c4de9d13c09bda8e1a7ffe46b2e7469
+size 2012
