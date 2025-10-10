54e027f532dfa5443fd5096c42f9fbf847ffb20f
DESKTOP-PFILKRM\marz anoya <espie.37743@gmail.com>
2025-10-10 22:17:53 +0800

    Items being consumable has been implemented...

--- Assets/Script/Kirgya/Dungeon/NoviceDungeon/NoviceDungeonManager.cs
+++ Assets/Script/Kirgya/Dungeon/NoviceDungeon/NoviceDungeonManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0757afc33725c8197757b5c6824bce6b931ea9795b29da6d1ccf792d8532d915
-size 19282
+oid sha256:167e1ffccb079f24afef332d61a7e817c8269bbc979ad706993bb25976f49818
+size 21210
