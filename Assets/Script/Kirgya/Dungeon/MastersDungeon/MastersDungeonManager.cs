12de1fc593a055618ddf9b732b2a42fd5f49d684
DESKTOP-PFILKRM\marz anoya <espie.37743@gmail.com>
2025-10-12 12:14:34 +0800

    Done integrating assets for enemies and bosses...

--- Assets/Script/Kirgya/Dungeon/MastersDungeon/MastersDungeonManager.cs
+++ Assets/Script/Kirgya/Dungeon/MastersDungeon/MastersDungeonManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf317d569a755372e1eb6d32e13a39f8e294730bfbc0a39b98811ea6d8e0b5ac
-size 22399
+oid sha256:df2e6fc956a101735f533e43e86ba1c8038eb1ea1acae247a92077f04b8d9105
+size 21371
--- Assets/Script/Kirgya/Dungeon/NoviceDungeon/NoviceDungeonManager.cs
+++ Assets/Script/Kirgya/Dungeon/NoviceDungeon/NoviceDungeonManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4721cae2b7c38c147abba748b78c68c0b2ba46a10d49e29ff0fad22b8d89d67
-size 22842
+oid sha256:3c9b8e5442471530189690ad5519fb53bb4c044148080fd1cc5ae57f0f1e1c74
+size 21401
