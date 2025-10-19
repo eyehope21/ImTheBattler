c96769826e6e561e6d4cee928450b2212b9a5fc9
DESKTOP-PFILKRM\marz anoya <espie.37743@gmail.com>
2025-10-20 06:15:13 +0800

    Win/Defeat panel has been implemented. 75% debugged...

--- Assets/Script/Kirgya/Multiplayer/MultiplayerBattleManager.cs
+++ Assets/Script/Kirgya/Multiplayer/MultiplayerBattleManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d96add25975dc8ca3d7f3733c1b9dc6925576678582ddeb51980df761f97eaf3
-size 20816
+oid sha256:a35bbed536e8c901851e91bba96ceb13a2032a5baaaa797b1ac3902aaa827528
+size 23520
