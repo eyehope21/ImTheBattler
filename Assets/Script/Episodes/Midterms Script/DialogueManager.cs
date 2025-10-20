9f77edac8d230b7b9a6b494007c22722f847d5b6
kujojotarooo0000-admin <kujojotarooo0000@gmail.com>
2025-10-21 03:09:29 +0800

    StoryMode Comporg1 Midterms- Novice Underconstruction

---
+++ Assets/Script/Episodes/Midterms Script/DialogueManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:76a72a0fbfac23f4c11304917a3d365c72d9912589d66babb5251d545876a5fb
+size 1744
---
+++ Assets/Script/MailManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:83b599540b0d50d99eb3eb683b173062d3fc40e67db56ea81b118aff277115ef
+size 5528
