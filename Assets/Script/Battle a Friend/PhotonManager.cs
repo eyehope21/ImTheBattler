3bca64a63d9f6825c153eaab89a35bb52e74bfcb
DESKTOP-PFILKRM\marz anoya <espie.37743@gmail.com>
2025-10-22 05:25:23 +0800

    Completed the AR attack slash in multiplayer but still untested...

--- Assets/Script/Battle a Friend/PhotonManager.cs
+++ Assets/Script/Battle a Friend/PhotonManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d29c27352ad8f1a3f88e295cd666837cca5ea9e8def57663606b5080ddc43b22
-size 7014
+oid sha256:a95b74262c3944de106e7f6eb1c747e0ce3d591f91e9f563a1faf7157e0f7e80
+size 7018
--- Assets/Script/Kirgya/Multiplayer/MultiplayerBattleManager.cs
+++ Assets/Script/Kirgya/Multiplayer/MultiplayerBattleManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3c24b3306c14d767f5d0b6c0f65f847a2ab215edd87429a3da163a43e6d9463
-size 22890
+oid sha256:50713da1a7886daad150ef243f47d3951da80c47f972f952c6e97968e7b1edac
+size 24885
