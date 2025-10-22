68d30de6ec45699b019cc0f013a7658545a688fd
DESKTOP-PFILKRM\marz anoya <espie.37743@gmail.com>
2025-10-22 23:08:17 +0800

    Fixed the spawner problem...

--- Assets/Script/Kirgya/Dungeon/MastersDungeon/MastersDungeonManager.cs
+++ Assets/Script/Kirgya/Dungeon/MastersDungeon/MastersDungeonManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc64fb57ccf82a91cd4496c809d16fcc71885be50b37b5a93e3f8784a82c9960
-size 22547
+oid sha256:4f4944d6df60956f262d78d59dcca09c276d9cd05016c4c9079f74e200c3eeff
+size 21159
