7940de8a9216e741412f16a6a3eaea7d4ddbf9c0
DESKTOP-PFILKRM\marz anoya <espie.37743@gmail.com>
2025-11-01 21:19:28 +0800

    Checkpoint 3

--- Assets/Script/Kirgya/Dungeon/NoviceDungeon/NoviceDungeonQuizManager.cs
+++ Assets/Script/Kirgya/Dungeon/NoviceDungeon/NoviceDungeonQuizManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef4cd82dfec2ba822a948b24ac2f9f6e2790e57be7246fbf0127a710f8028607
-size 17868
+oid sha256:84e87ee82ae82054390a5b47772bd41c7e61b213d41024abe53a7b1ab65f8620
+size 18971
