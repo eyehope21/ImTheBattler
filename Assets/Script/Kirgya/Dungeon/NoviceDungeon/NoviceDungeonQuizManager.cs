99331122acc033aa849324e278fa164a15ee3ee9
DESKTOP-PFILKRM\marz anoya <espie.37743@gmail.com>
2025-11-03 21:13:10 +0800

    Checkpoint 5. Gumana na rin sa wakas kaso pang Novice palang...

--- Assets/Script/Kirgya/Dungeon/NoviceDungeon/NoviceDungeonQuizManager.cs
+++ Assets/Script/Kirgya/Dungeon/NoviceDungeon/NoviceDungeonQuizManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84e87ee82ae82054390a5b47772bd41c7e61b213d41024abe53a7b1ab65f8620
-size 18971
+oid sha256:19185c0ae85bf5d387507988224a98cd94fa820ec90908f37cf5c305ed17f31e
+size 19263
