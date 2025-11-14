f28c6214f70a8c9d9d096fb38d91a6148af99f87
kujojotarooo0000-admin <kujojotarooo0000@gmail.com>
2025-11-15 07:40:17 +0800

    Modiifed UI , fonts google sign in

--- Assets/Script/GoogleSignInHandler.cs
+++ Assets/Script/GoogleSignInHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5aa3d7d2d1ea92da9968cb37270b74946f02801658e409b6b87469a36f63837e
+oid sha256:59addccfa939efad2771307ca117616c3b4f76a4e5d9381acd43ed138b670eea
 size 3694
--- Assets/Script/Portalscript/PortalManager.cs
+++ Assets/Script/Portalscript/PortalManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d237e146f2454b8b2df0d6bf54f3531008c83a47c5fb6772776d424cac58e4b
-size 9576
+oid sha256:a143b104ec877de2567d042b3939563a9081c07e21a5b8055c9d4b5c7dc06b64
+size 9609
