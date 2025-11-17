a1dd0050dcea1bb4778458d809b044f9db84a6d1
HL-202306061624\Administrator <eyehope21@gmail.com>
2025-11-17 12:42:20 +0800

    Updated googlesign in not sure if its work

---
+++ Assets/Editor/GoogleSignInAndroidFix.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6242776ce9a571c161869cae010216dc5d28434a4247b977a56155c6d1a5c466
+size 1934
--- Assets/Script/GoogleSignInHandler.cs
+++ Assets/Script/GoogleSignInHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3bd78596c41fb4793ef5aae7e4af40111edf22bc08487c0098f05ca4975b5024
-size 3694
+oid sha256:13a217e9a5f93e88e681e22b79ac164a56c323cb5845d297522a0b72b613f377
+size 6965
